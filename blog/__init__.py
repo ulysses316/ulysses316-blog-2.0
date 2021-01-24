06512463850199181c7087d47248b7b8baef2f38
ulysses316 <ulises316@live.com.mx>
2021-01-24 04:50:19 -0600

    [MOD] database var envioron

--- blog/__init__.py
+++ blog/__init__.py
@@ -13,7 +13,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = os.environ.get('SECRET_KEY'),
-        SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI'),
+        SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         UPLOAD_FOLDER = os.path.join(app.static_folder, 'upload'),
         MAIL_SERVER='smtp.googlemail.com',
